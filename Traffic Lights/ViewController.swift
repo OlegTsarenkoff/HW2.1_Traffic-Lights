c4077e99df5bf4d3e79eb3ff5a6b2adae45c6b0c
Oleg Tsarenkoff <tsarenkow@icloud.com>
2021-05-19 17:51:13 +0300

    Add mechanic turn lights

--- Traffic Lights/ViewController.swift
+++ Traffic Lights/ViewController.swift
@@ -7,13 +7,64 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+enum CurrentLight {
+    case red, yellow, green
+}
 
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var redLights: UILabel!
+    @IBOutlet weak var yellowLights: UILabel!
+    @IBOutlet weak var greenLights: UILabel!
+    
+    @IBOutlet weak var startButton: UIButton!
+    
+    private var currentLight = CurrentLight.red
+    private let lightIsOn: CGFloat = 1.0
+    private let lightIsOff: CGFloat = 0.3
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        startButton.layer.cornerRadius = 10
+        
+        redLights.alpha = lightIsOff
+        yellowLights.alpha = lightIsOff
+        greenLights.alpha = lightIsOff
+        
+        redLights.layer.masksToBounds = true
+        redLights.layer.cornerRadius = 20
+        
+        yellowLights.layer.masksToBounds = true
+        yellowLights.layer.cornerRadius = 20
+        
+        greenLights.layer.masksToBounds = true
+        greenLights.layer.cornerRadius = 20
+        
     }
-
-
+    
+    @IBAction func tappedNextButton() {
+        startButton.setTitle("Next", for: .normal)
+        
+        switch currentLight {
+        case .red:
+            redLights.alpha = lightIsOn
+            yellowLights.alpha = lightIsOff
+            greenLights.alpha = lightIsOff
+            currentLight = .yellow
+        case .yellow:
+            redLights.alpha = lightIsOff
+            yellowLights.alpha = lightIsOn
+            greenLights.alpha = lightIsOff
+            currentLight = .green
+        case .green:
+            redLights.alpha = lightIsOff
+            yellowLights.alpha = lightIsOff
+            greenLights.alpha = lightIsOn
+            currentLight = .red
+        }
+        
+    }
+    
 }
 
